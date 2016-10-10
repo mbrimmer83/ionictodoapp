19c94aae9be4fe290436811df8c7589bbdef4f45
Matthew <mbrimmer1@gmail.com>
2016-10-09 21:29:41 -0400

    More updates

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -1,73 +1,119 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+var app = angular.module('starter', ['ionic', 'ngCordova', 'ngStorage']);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
-})
+});
 
-.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-
     .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
   })
-
-  .state('app.search', {
-    url: '/search',
+  .state('app.map', {
+    url: '/map',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/map.html',
+        controller: 'MapController'
       }
     }
   })
-
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.home', {
+      url: '/home',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/home.html'
         }
       }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+    });
+  $urlRouterProvider.otherwise('/app/home');
+});
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+app.controller('AppCtrl', function($scope, $ionicModal, $timeout, geoCode) {
+  $scope.markerData = {};
+  $ionicModal.fromTemplateUrl('templates/marker.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+
+  $scope.closeMarker = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.makeMarker = function() {
+    $scope.modal.show();
+  };
+
+  $scope.doCoords = function() {
+    console.log("Looking up address", $scope.markerData);
+    $timeout(function() {
+      geoCode.getCoords($scope.markerData)
+      $scope.closeMarker();
+    }, 1000);
+  };
+});
+
+app.service('googleMap', function() {
+  var data ={};
+  this.makeMarker = function(coords, map) {
+    var marker = new google.maps.Marker({
+          position: coords,
+          map: map
+    });
+    return marker;
+  };
+});
+
+app.service('geoCode', function() {
+  this.getCoords = function(data) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': data.address}, function(results, status) {
+      if (status == 'OK') {
+        var latitude = results[0].geometry.location.lat();
+        var longitude = results[0].geometry.location.lng();
+        console.log(latitude);
+        console.log(longitude);
+      } else {
+        console.log("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  };
+});
+
+app.controller('MapController', function($scope, $state, $cordovaGeolocation, googleMap, $ionicLoading) {
+  console.log("MainController is active");
+  var coords = {};
+  var positionOptions = {timeout: 10000, enableHighAccuracy: true};
+  $ionicLoading.show();
+  $cordovaGeolocation.getCurrentPosition(positionOptions)
+  .then(function(position) {
+    coords = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    var mapElement = document.getElementById('map');
+    var mapOptions = {
+      center: coords,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    $ionicLoading.hide();
+    var map = new google.maps.Map(mapElement, mapOptions);
+    googleMap.makeMarker(coords, map);
+  });
 });
